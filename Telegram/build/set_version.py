17db5d9067a8ec9d7ab250cd90a9cf777ac98eba
KUKURUZKA <60149954+KUKURUZKA165@users.noreply.github.com>
2023-08-16 15:42:16 +0300

    version tool update

--- Telegram/build/set_version.py
+++ Telegram/build/set_version.py
@@ -14,17 +14,17 @@ def replaceline(path, linecontains, replacewith):
                 file.write(line)
 
 major, minor, patch, materialver = input("Enter your major verion: "), input("Minor: "), input("Patch: "), input("Material Telegram version: ")
-ver = major + "." + minor + "." + patch + "-" + materialver
+ver = major + "." + minor + "." + patch + "." + materialver
 print(ver)
 with open("version", 'xt') as versionfile:
     versionfile.write(f"AppVersion         {int(major) * 100}{int(minor)*10}{patch}{materialver}\n"
                       f"AppVersionStrMajor {major}.{minor}\n"
-                      f"AppVersionStrSmall {major}.{minor}.{patch}-{materialver}\n"
-                      f"AppVersionStr      {major}.{minor}.{patch}-{materialver}\n"
+                      f"AppVersionStrSmall {major}.{minor}.{patch}.{materialver}\n"
+                      f"AppVersionStr      {major}.{minor}.{patch}.{materialver}\n"
                       f"BetaChannel        0\nAlphaVersion       0\n"
-                      f"AppVersionOriginal {major}.{minor}.{patch}-{materialver}\n")
+                      f"AppVersionOriginal {major}.{minor}.{patch}.{materialver}\n")
 replaceline("../SourceFiles/core/version.h", 'constexpr auto AppVersionStr = "',
-            f'{major}.{minor}.{patch}-{materialver}";')
+            f'{major}.{minor}.{patch}.{materialver}";')
 replaceline("../SourceFiles/core/version.h", 'constexpr auto AppVersion = ',
             f'{int(major) * 100}{int(minor)*10}{patch}{materialver};')
 replaceline("../Resources/winrc/Telegram.rc", ' FILEVERSION ',
@@ -36,6 +36,10 @@ replaceline("../Resources/winrc/Updater.rc", ' FILEVERSION ',
 replaceline("../Resources/winrc/Updater.rc", ' PRODUCTVERSION ',
             f'{major},{minor},{patch},{materialver}')
 replaceline("../Resources/winrc/Telegram.rc", '            VALUE "FileVersion", "',
-            f'{major}.{minor}.{patch}-{materialver}"')
+            f'{major}.{minor}.{patch}.{materialver}"')
 replaceline("../Resources/winrc/Updater.rc", '            VALUE "FileVersion", "',
-            f'{major}.{minor}.{patch}-{materialver}"')
+            f'{major}.{minor}.{patch}.{materialver}"')
+replaceline("../Resources/winrc/Telegram.rc", '            VALUE "ProductVersion", "',
+            f'{major}.{minor}.{patch}.{materialver}"')
+replaceline("../Resources/winrc/Updater.rc", '            VALUE "ProductVersion", "',
+            f'{major}.{minor}.{patch}.{materialver}"')
