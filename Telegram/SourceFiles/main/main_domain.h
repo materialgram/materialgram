1dffc2945a7b7262eb27142c5ff97031b4836e69
kukuruzka <60149954+kukuruzka165@users.noreply.github.com>
2024-01-08 19:22:25 +0200

    increase max accounts limit

--- Telegram/SourceFiles/main/main_domain.h
+++ Telegram/SourceFiles/main/main_domain.h
@@ -30,8 +30,8 @@ public:
 		std::unique_ptr<Account> account;
 	};
 
-	static constexpr auto kMaxAccounts = 3;
-	static constexpr auto kPremiumMaxAccounts = 6;
+	static constexpr auto kMaxAccounts = 10;
+	static constexpr auto kPremiumMaxAccounts = 20;
 
 	explicit Domain(const QString &dataName);
 	~Domain();
