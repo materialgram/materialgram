231569ff70ce7e7ad8229836801e470e33333445
Omansh Krishn <omansh@duck.com>
2025-06-28 18:31:49 +0530

    use cmake install to create install dir (#65)

--- Telegram/build/docker/centos_env/build.sh
+++ Telegram/build/docker/centos_env/build.sh
@@ -4,3 +4,5 @@ set -e
 cd Telegram
 ./configure.sh "$@"
 cmake --build ../out --config "${CONFIG:-Release}"
+
+([[ -d ../out/install ]] && rm -rf ../out/install; mkdir -p ../out/install) && DESTDIR=../out/install cmake --install ../out
