ace42226b6c155bc9856e78ac60b61f2181e30e9
23rd <23rd@vivaldi.net>
2025-10-30 18:49:41 +0300

    Fix rounding in preview of settings for quick dialog actions.

--- Telegram/SourceFiles/settings/settings_chat.cpp
+++ Telegram/SourceFiles/settings/settings_chat.cpp
@@ -1331,6 +1331,7 @@ void SetupChatListQuickAction(
 		}, widget->lifetime());
 		widget->paintRequest() | rpl::start_with_next([=] {
 			auto p = QPainter(widget);
+			auto hq = PainterHighQualityEnabler(p);
 
 			const auto height = st::dialogsRowHeight;
 			const auto actionWidth = st::dialogsQuickActionRippleSize * 0.75;
@@ -1359,7 +1360,6 @@ void SetupChatListQuickAction(
 			const auto label = actionToLabel(group->current());
 			const auto isDisabled = (label == LabelType::Disabled);
 
-			auto hq = PainterHighQualityEnabler(p);
 			p.fillRect(
 				QRect(0, 0, rect::right(rect), st::lineWidth),
 				st::windowBgOver);
