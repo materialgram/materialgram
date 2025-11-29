fd52b3c23bb74388bf46bf85832bd8e4aac25137
23rd <23rd@vivaldi.net>
2025-11-29 08:17:50 +0300

    Fixed once again unwanted reset filter on creation of tabs strip.
    Regression was introduced in c0bbb669e0.

--- Telegram/SourceFiles/dialogs/dialogs_widget.cpp
+++ Telegram/SourceFiles/dialogs/dialogs_widget.cpp
@@ -1677,6 +1677,7 @@ void Widget::toggleFiltersMenu(bool enabled) {
 
 		_chatFilters = base::make_unique_q<NoScrollPropagationWidget>(this);
 		const auto raw = _chatFilters.get();
+		const auto idBeforeTabs = controller()->activeChatsFilterCurrent();
 		const auto inner = Ui::AddChatFiltersTabsStrip(
 			_chatFilters.get(),
 			&session(),
@@ -1689,6 +1690,9 @@ void Widget::toggleFiltersMenu(bool enabled) {
 			Window::GifPauseReason::Any,
 			controller(),
 			true);
+		if (controller()->activeChatsFilterCurrent() != idBeforeTabs) {
+			controller()->setActiveChatsFilter(idBeforeTabs);
+		}
 		raw->show();
 		raw->stackUnder(_scroll);
 		raw->resizeToWidth(width());
